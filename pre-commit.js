ecee094fd9f5493ccf0aad88e3ba9bf84c4d12d9
Matthew Chase Whittemore <matthew@socialtables.com>
2013-08-03 19:27:24 -0400

    fail

--- pre-commit.js
+++ pre-commit.js
@@ -3,10 +3,13 @@ var exec = require('child_process').exec;
 exec("./node_modules/mocha/bin/mocha --reporter json", function(err, stderr, stdout){
 	var json = JSON.parse(stderr);
 	
-	if(json.stats.pending+json.stats.failure>0){
-		process.exit(1);
-	}
-	else{
-		process.exit(0);
-	}
+	// if(json.stats.pending+json.stats.failure>0){
+	// 	console.log(json);
+	// 	process.exit(1);
+	// }
+	// else{
+	// 	process.exit(1);
+	// }
+	console.log(json);
+	process.exit(1);
 })
