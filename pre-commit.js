d61de47c8389646570a8acf9ce6ee1b9b8b9272b
Matthew Chase Whittemore <matthew@socialtables.com>
2013-08-03 20:31:50 -0400

    will this work?

--- pre-commit.js
+++ pre-commit.js
@@ -3,12 +3,68 @@ var fs = require('fs');
 
 var reportFile = __dirname+"/test/test-report.json";
 
-
 var newTests = false;
 var oldTests = false;
 
 
-var toDictionary = function(test_report){
+
+var run = function(){
+	runTests();
+	loadTestReport();
+}
+
+var runTests = function(){
+	exec("./node_modules/mocha/bin/mocha --reporter json", function(err, stderr, stdout){
+		var testJson = JSON.parse(stderr);
+		
+		if(testJson.stats.failures>0){
+			process.exit(1);
+		}
+
+		newTests = testJsonToTestReport(testJson);
+
+		if(oldTests!=false){
+			compareReports();
+		}
+	});
+}
+
+var loadTestReport = function(){
+	fs.readFile(reportFile, function(err, data){
+
+		if(err){
+			if(err.code=="ENOENT"){
+				oldTests = {};
+			}
+			else{
+				console.error("FILE READ ERROR WITH `test-report.json`", err);
+				process.exit(1);
+			}
+		}
+		else{
+			if(data==""){
+				oldTests = {};
+			}
+			else{
+				try{
+					oldTests=JSON.parse(data);
+				}
+				catch(err){
+					console.error("JSON PARSE ERROR with `test-report.json`", err);
+					process.exit(1);
+				}
+			}
+		}
+
+		if(newTests!=false){
+			compareReports();
+		}
+
+	});
+}
+
+
+var testJsonToTestReport = function(test_report){
 	var report = {};
 
 	var iTests = test_report.stats.tests;
@@ -38,21 +94,7 @@ var toDictionary = function(test_report){
 	return report;
 }
 
-var save = function(){
-	fs.writeFile(reportFile, JSON.stringify(newTests), function(err){
-
-		if(err){
-			console.error("FAILED TO SAVE TEST REPORT", err);
-			process.exit(1);
-		}
-		else{
-			process.exit(0);
-		}
-
-	});
-}
-
-var judge = function(){
+var compareReports = function(){
 
 	var failed = false;
 	
@@ -97,56 +139,22 @@ var judge = function(){
 		process.exit(1);
 	}
 	else{
-		save();
+		saveTestReport();
 	}
 
 }
 
-
-
-exec("./node_modules/mocha/bin/mocha --reporter json", function(err, stderr, stdout){
-	var test_report = JSON.parse(stderr);
-	
-	if(test_report.stats.failures>0){
-		process.exit(1);
-	}
-
-	newTests = toDictionary(test_report);
-
-	if(oldTests!=false){
-		judge();
-	}
-});
-
-
-fs.readFile(reportFile, function(err, data){
-
-	if(err){
-		if(err.code=="ENOENT"){
-			oldTests = {};
-		}
-		else{
-			console.error("FILE READ ERROR WITH `test-report.json`", err);
+var saveTestReport = function(){
+	fs.writeFile(reportFile, JSON.stringify(newTests), function(err){
+		if(err){
+			console.error("FAILED TO SAVE TEST REPORT", err);
 			process.exit(1);
 		}
-	}
-	else{
-		if(data==""){
-			oldTests = {};
-		}
 		else{
-			try{
-				oldTests=JSON.parse(data);
-			}
-			catch(err){
-				console.error("JSON PARSE ERROR with `test-report.json`", err);
-				process.exit(1);
-			}
+			process.exit(0);
 		}
-	}
+	});
+}
 
-	if(newTests!=false){
-		judge();
-	}
 
-});
+run();
