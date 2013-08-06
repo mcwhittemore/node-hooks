6aa94f712053ae2a0f9fdc6cce94892e3b33c930
Matthew Chase Whittemore <matthew@socialtables.com>
2013-08-06 00:50:17 -0400

    fixing cli name and flushing out hooks.router

--- bin/hooks.js
+++ bin/hooks.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 ;(function () { // wrapper in case we're in module_context mode
 
+	var commands = [
+		"help",
+		"init"
+	]
 
 	var run = function(args){
+		if(commands.indexOf(args[0])!=-1){
+			require("./commands/"+args[0])(args.slice(1));
+		}
+		else{
+			console.error("`"+args[0]+"` is not a valid command");
+			require("./commands/help")();
+		}
 	}
 
-	process.title = "chicken";
+	process.title = "hooks";
 
 	var userArgs = process.argv.slice(2);
 
@@ -15,4 +26,5 @@
 	else{
 		run(userArgs);
 	}
-}
+
+})();
