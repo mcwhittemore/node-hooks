d60935af358aeb63f1ac85ad55c4c0a1739fdee5
Matthew Chase Whittemore <matthew@socialtables.com>
2013-09-16 23:49:10 -0400

    adding comments

--- configure.js
+++ configure.js
@@ -4,6 +4,7 @@ if(process.env.npm_config_global!=="true"){
 	var fs = require("fs");
 
 	if(fs.existsSync(process.cwd()+"/../.bin/hooks")){
+		//being installed a project folder
 		var exec = require("child_process").exec;
 		var command = "cd ../.. && "+process.cwd()+"/../.bin/hooks "+process.argv[2];
 		exec(command, function(err, stdout, stderr){
@@ -15,6 +16,7 @@ if(process.env.npm_config_global!=="true"){
 		});
 	}
 	else{
+		//being installed itself
 		require(process.cwd()+"/commands/"+process.argv[2])([]);
 	}
 
