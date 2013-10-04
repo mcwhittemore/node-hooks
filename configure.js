aa9f68a656469b0fc3137b0757ba5dcba54b4b49
Matthew Chase Whittemore <matthew@socialtables.com>
2013-10-04 01:25:02 -0400

    none working implementation of the spawn style command

--- configure.js
+++ configure.js
@@ -3,22 +3,46 @@ if(process.env.npm_config_global!=="true" && process.env.NODE_HOOKS != "DO_NOT_I
 
 	var fs = require("fs");
 
+	var spawn = require("child_process").spawn;
+	
+	var cwd, file;
+
 	if(fs.existsSync(process.cwd()+"/../.bin/hooks")){
 		//being installed a project folder
-		var exec = require("child_process").exec;
-		var command = "cd ../.. && "+process.cwd()+"/../.bin/hooks "+process.argv[2];
-		exec(command, function(err, stdout, stderr){
-			console.log(stdout);
-			console.log(stderr);
-			if(err){
-				process.exit(err.code);
-			}
+		cwd = process.cwd()+"/../..";
+		file = process.cwd()+"/.bin/hooks";
+	}
+	else{
+		//being installed inside itself
+		cwd = process.cwd();
+		file = process.cwd()+"/.bin/hooks";
+	}
+
+	console.log("file", file);
+	if(fs.existsSync(file)){
+		console.log("CWD", cwd);
+		var child = spawn("node", [file, process.argv[2]], cwd);
+
+		child.stderr.on("data", function(data){
+			process.stderr.write(data);
+		});
+
+		child.stdout.on("data", function(data){
+			process.stdout.write(data);
+		});
+
+		child.on("error", function(err){
+			console.error("HOOKS:", err.message);
+		})
+
+		child.on("close", function(code){
+			console.log("CLOSING");
+			process.exit(code);
 		});
 	}
 	else{
-		//being installed itself
-		require(process.cwd()+"/commands/"+process.argv[2])([]);
+		console.log("hhhhhh");
+		process.exit(1);
 	}
-
 	
 }
